23ac218f7b7dbe20b99cda00cf137a61aa90ab60
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-03-23 22:26:42 +0100

    schema created and Teacher class added

---
+++ src/main/java/pl/edu/agh/mwo2017/bockowsk/Teacher.java
@@ -0,0 +1,86 @@
+package pl.edu.agh.mwo2017.bockowsk;
+
+import java.util.Set;
+
+public class Teacher {
+// PROPERTIES
+	private long id;
+	private String name;
+	private String surname;
+	private String title;
+	private String pesel;
+
+	private Set<SchoolClass> charges;
+
+// CONSTRUCTORS
+	public Teacher(String name, String surname, String title, String pesel) {
+		this();
+		this.name = name;
+		this.surname = surname;
+		this.title = title;
+		this.pesel = pesel;
+	}
+
+	public Teacher() {
+		super();
+	}
+// GETTERS & SETTERS
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPesel() {
+		return pesel;
+	}
+
+	public void setPesel(String pesel) {
+		this.pesel = pesel;
+	}
+
+	public Set<SchoolClass> getLectures() {
+		return charges;
+	}
+
+	public void setLectures(Set<SchoolClass> lectures) {
+		this.charges = lectures;
+	}
+
+	@Override
+	public String toString() {
+		return "Teacher [id=" + id + ", name=" + name + ", surname=" + surname + ", title=" + title + ", pesel=" + pesel
+				+ ", lectures=" + charges + "]";
+	}
+	
+// HELPERS
+	
+	
+}
